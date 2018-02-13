f3868476ba6f2aeb59a98f7afaafcef3815db2ff
Jim Kang <jimkang@gmail.com>
2018-02-13 13:28:02 -0500

    lol

---
+++ init.js
@@ -0,0 +1,52 @@
+(function init() {
+
+  NodeList.prototype.forEach = function nodeListEach(callback, thisArg) {
+    for (var i = 0; i < this.length; ++i) {
+      var value = this[i];
+      if (thisArg) {
+        callback.bind(thisArg)(value, i, this);
+      }
+      else {
+        callback(value, i, this);
+      }
+    }
+  };
+
+  function elIsInView(viewTop, viewBottom, el) {
+    var topThreshold = el.offsetTop + el.offsetHeight * 0.33;
+    var bottomThreshold = el.offsetTop + el.offsetHeight * 0.67;
+    return !(bottomThreshold < viewTop || topThreshold > viewBottom);
+  }
+
+  var scrollWatcher = createScrollWatcher();
+  scrollWatcher.watchElements(
+    [
+      {
+        selector: '.get-one', 
+        isInViewFunction: elIsInView
+      },
+      {
+        selector: '.get-two', 
+        isInViewFunction: elIsInView
+      },
+      {
+        selector: '.get-three', 
+        isInViewFunction: elIsInView
+      },
+      {
+        selector: '.get-final', 
+        isInViewFunction: elIsInView
+      }
+    ]
+  );
+
+  document.addEventListener('elMovedIntoView', 
+    function onElCenterInView(e) {
+      e.detail.classList.add('in-spotlight');
+    }
+  );
+
+  // Initial sync of what's in-view and the in-spotlight class.
+  scrollWatcher.respondToScroll();
+
+})();
